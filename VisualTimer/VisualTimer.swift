b53a946850a8b0421dd0a3f1cb189e6249037b1e
Zach Strenfel <zwstrenfel@gmail.com>
2017-03-20 10:13:41 -0700

    first commit

---
+++ VisualTimer/VisualTimer.swift
@@ -0,0 +1,21 @@
+//
+//  VisualTimer.swift
+//  VisualTimer
+//
+//  Created by Zach Strenfel on 3/20/17.
+//  Copyright © 2017 Zach Strenfel. All rights reserved.
+//
+
+import UIKit
+
+class VisualTimer: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
